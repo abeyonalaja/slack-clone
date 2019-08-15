33d00ee4a03759ea8aefbe509e336d05c20223f9
Abey Onalaja <abey.onalaja@gmail.com>
2019-08-15 22:52:29 +0100

    Actually added files

---
+++ web/src/containers/NotFound/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NotFound = () => (
+  <div style={{ margin: "2rem auto", textAlign: "center" }}>
+    <p>Page not found</p>
+    <p>sdfsad</p>
+  </div>
+);
+
+export default NotFound;
