c5e44ecf23f5bc5110dcbb7b07f4ce72259197c2
Abey Onalaja <abey.onalaja@gmail.com>
2019-08-16 20:15:18 +0100

    update to session controller

--- api/lib/sling_web/controllers/session_controller.ex
+++ api/lib/sling_web/controllers/session_controller.ex
@@ -9,19 +9,39 @@ defmodule SlingWeb.SessionController do
       {:ok, user} ->
         conn = Guardian.Plug.sign_in(conn, user)
         token = Guardian.Plug.current_token(conn)
-
-        IO.puts("got USer")
-
         conn
         |> put_status(:created)
         |> render("show.json", user: user, token: token)
-
       {:error, _} ->
-        IO.puts("NO USER FOUND")
-
         conn
         |> put_status(:unauthorized)
         |> render("error.json", error: "email or password")
+    end
+  end
+
+  def delete(conn, _) do
+    jwt = Guardian.Plug.current_token(conn)
+    Guardian.Plug.revoke!(jwt)
+
+    conn
+    |> put_status(:ok)
+    |> render("delete.json")
+  end
+
+  def refresh(conn, _params) do
+    user = Guardian.Plug.current_resource(conn)
+    jwt = Guardian.Plug.current_token(conn)
+    {:ok, claims} = Guardian.Plug.claims(conn)
+
+    case Guardian.refresh!(jwt, claims, %{ttl: {30, :days}}) do
+      {:ok, new_jwt, _new_claims} ->
+        conn
+        |> put_status(:ok)
+        |> render("show.json", user: user, jwt: new_jwt)
+      {:error, _reason} ->
+        conn
+        |> put_status(:unauthorized)
+        |> render("forbidden.json", error: "Not authenticated")
     end
   end
 
